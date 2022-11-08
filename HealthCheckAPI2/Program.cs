707b69b2f44fbcd9b5a7afe180deeb9ac1674cfa
Kevin <kwcw@live.com>
2022-11-08 20:57:47 +1100

    Initial Commit

---
+++ HealthCheckAPI2/Program.cs
@@ -0,0 +1,32 @@
+using HealthCheckAPI2;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks()
+    .AddCheck("ICMP_01", new ICMPHealthCheck("www.google.com", 100))
+    .AddCheck("ICMP_02", new ICMPHealthCheck("www.ryadel.com", 100))
+    .AddCheck("ICMP_03", new ICMPHealthCheck("q12332", 100));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseHealthChecks(new PathString("/api/health"));
+app.MapControllers();
+
+app.Run();
